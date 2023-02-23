536b1a6d12bddee51123f1e28f9052f42c0e9ac6
sokolovigor210 <117148070+sokolovigor210@users.noreply.github.com>
2023-02-24 02:56:03 +0300

    Add files via upload

---
+++ lab1/16.02 task 2 perebor.cpp
@@ -0,0 +1,60 @@
+﻿#include <iostream>
+#include <chrono>
+#include <random>
+
+int main_array[500000];
+int arr[500000];
+
+int sum_perebor(int n, int key)
+{
+	int f_idx_res = 0;
+	int s_idx_res = 0;
+	bool found = false;
+	for (int f_idx = 0; f_idx < n && !found; f_idx++)
+	{
+		for (int s_idx = f_idx + 1; s_idx < n && !found; s_idx++)
+		{
+			if (main_array[f_idx] + main_array[s_idx] == key && f_idx != s_idx && 
+				main_array[f_idx] != main_array[s_idx])
+			{
+				std::cout << "Values: " << main_array[f_idx] << "and" << main_array[s_idx] << std::endl;
+				found = true;
+			}
+		}
+	}
+	if (found == false)
+	{
+		std::cout << "This pair doesn't exist" << std::endl;
+	}
+	return -1;
+}
+
+int main()
+{
+	for (int j = 0; j < 500000; j++)
+	{
+		arr[j] = j;
+	}
+	unsigned seed = 500;
+	std::default_random_engine rng(seed);
+	std::uniform_int_distribution<unsigned> dstr(0, 500000);
+
+	for (int n = 0; n <= 50000; n += 5000)
+	{
+		int const amount(5);
+		auto begin = std::chrono::steady_clock::now();
+		for (unsigned cnt = 0; cnt != amount; ++cnt)
+		{
+			for (int s = 0; s < n; s++)
+			{
+				main_array[s] = arr[dstr(rng)] % n;
+			}
+
+			int number = 3 * n;
+			sum_perebor(n, number);
+		}
+		auto end = std::chrono::steady_clock::now();
+		auto time_span = std::chrono::duration_cast<std::chrono::milliseconds>(end - begin);
+		std::cout << n << " " << time_span.count() << std::endl;
+	}
+}
---
+++ lab1/16.02 №1 bin poisk.cpp
@@ -0,0 +1,72 @@
+﻿#include <iostream>
+#include <chrono>
+#include <random>
+
+int main_array[1000000];
+int arr[1000000];
+
+int bin_search(int* arr, int len, int key)
+{
+	int l_idx = 0;
+	int r_idx = len - 1;
+	while (l_idx <= r_idx)
+	{
+		int median = (l_idx + r_idx) / 2;
+		if (main_array[median] > key)
+		{
+			r_idx = median - 1;
+		}
+
+		else if (main_array[median] < key)
+		{
+			l_idx = median + 1;
+		}
+		else
+		{
+			return median;
+		}
+	}
+	return len;
+}
+
+bool line_search(int* arr, int len, int key)
+{
+	for (unsigned idx = 0; idx != len; idx++)
+	{
+		if (arr[idx] == key)
+		{
+			return true;
+		}
+	}
+	return false;
+}
+
+int main()
+{
+	for (int j = 0; j < 1000000; j++)
+	{
+		arr[j] = j;
+	}
+	unsigned seed = 50;
+	std::default_random_engine rng(seed);
+	std::uniform_int_distribution<unsigned> dstr(0, 1000000);
+
+	for (int n = 0; n <= 1000000; n += 10000)
+	{
+		for (int s = 0; s < n; s++)
+		{
+			main_array[s] = arr[dstr(rng)];
+		}
+		std::sort(main_array, main_array + n);
+		int const amount(20000);
+		auto begin = std::chrono::steady_clock::now();
+		for (unsigned cnt = 0; cnt != amount; ++cnt)
+		{
+			int number = arr[dstr(rng)];
+			bin_search(main_array, n, number);
+		}
+		auto end = std::chrono::steady_clock::now();
+		auto time_span = std::chrono::duration_cast<std::chrono::microseconds>(end - begin);
+		std::cout << n << " " << time_span.count() << std::endl;
+	}
+}
