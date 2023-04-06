52399f892a812c8bacc8ba1dbb69c8fa54d9313d
sokolovigor210 <117148070+sokolovigor210@users.noreply.github.com>
2023-04-06 03:06:44 +0300

    Add files via upload

---
+++ lab3/lab 3 task 1.cpp
@@ -0,0 +1,147 @@
+﻿#include <iostream>
+
+struct LinkedList
+{
+    int data;
+    LinkedList* next;
+};
+
+LinkedList* insert_after(LinkedList* const current)
+{
+    if (current == nullptr)
+    {
+        return new LinkedList{ 0, current };
+    }
+    else
+    {
+        auto new_node = new LinkedList{ 0, current->next };
+        current->next = new_node;
+        return new_node;
+    }
+}
+
+LinkedList* remove_after(LinkedList* const current)
+{
+    if (nullptr == current)
+    {
+        return current;
+    }
+    else
+    {
+        auto new_current = current->next;
+        current->next = new_current->next;
+        delete new_current;
+        return current->next;
+    }
+
+}
+
+void erase(LinkedList* const head)
+{
+    auto current = head;
+    auto prev = current;
+    while (current != nullptr)
+    {
+        /*printf("Deleting %d\n", current->data);*/
+        current = current->next;
+        delete prev;
+        prev = current;
+    }
+}
+LinkedList* reverse_rec(LinkedList* const head)
+{
+    if (nullptr == head)
+    {
+        return head;
+    }
+    if (nullptr == head->next)
+    {
+        return head;
+    }
+    LinkedList* new_head = reverse_rec(head->next);
+
+    head->next->next = head;
+    head->next = nullptr;
+    return new_head;
+
+}
+
+LinkedList* reverse_itr(LinkedList* const head)
+{
+    if (nullptr == head)
+    {
+        return head;
+    }
+    auto current = head;
+    LinkedList* prev_node = nullptr;
+    while (current != nullptr)
+    {
+        auto next_node = current->next;
+        current->next = prev_node;
+        prev_node = current;
+        current = next_node;
+    }
+    return prev_node;
+}
+
+LinkedList* enter_data(LinkedList* const head, int count)
+{
+    LinkedList* tail = head;
+    for (int i = 0; i < count - 1; i++)
+    {
+        tail = insert_after(tail);
+        std::cin >> tail->data;
+    }
+    return tail;
+}
+
+void print_list(LinkedList* const head)
+{
+    if (nullptr == head)
+    {
+        std::cout << "List is empty!" << std::endl;
+        return;
+    }
+    auto current = head;
+    while (current != nullptr)
+    {
+        std::cout << current->data << ' ';
+        current = current->next;
+    }
+    std::cout << std::endl;
+}
+
+void test(LinkedList* head, LinkedList* tail, int count)
+{
+    std::cout << "List: " << std::endl;
+    print_list(head);
+    std::cout << "After deleting an element: " << std::endl;
+    remove_after(head);
+    print_list(head);
+    std::cout << "Reverse by iterations: " << std::endl;
+    reverse_itr(head);
+    print_list(tail);
+    std::cout << "Reverse by recursion: " << std::endl;
+    reverse_rec(tail);
+    print_list(head);
+    std::cout << "Erasing list: " << std::endl;
+    erase(head);
+    head = nullptr;
+    print_list(head);
+}
+
+int main()
+{
+    int count = 0;
+    std::cout << "Enter nodes' number: " << std::endl;
+    std::cin >> count;
+    std::cout << "Enter" << ' ' << count - 1 << ' ' << "elements:" << std::endl;
+
+    auto head = insert_after(nullptr);
+    head->data = 0;
+
+    auto tail = enter_data(head, count);
+    test(head, tail, count);
+
+    return 0;
+}
---
+++ lab3/lab 3 task 2.cpp
@@ -0,0 +1,155 @@
+﻿#include <iostream>
+#include <chrono>
+#include <random>
+#include <math.h>
+
+struct LinkedList
+{
+    int data;
+    LinkedList* next;
+};
+
+LinkedList* insert_after(LinkedList* const current, int& count)
+{
+    if (current == nullptr)
+    {
+        return new LinkedList{ count, current };
+    }
+    else
+    {
+        auto new_node = new LinkedList{ count, current->next };
+        current->next = new_node;
+        return new_node;
+    }
+}
+
+LinkedList* remove_after(LinkedList* const current)
+{
+    if (nullptr == current)
+    {
+        return current;
+    }
+    else
+    {
+        auto new_current = current->next;
+        current->next = new_current->next;
+        delete new_current;
+        return current->next;
+    }
+}
+
+int decision_by_massives(int* array, int n, int key)
+{
+    int index = 0;
+    while (n > 1)
+    {
+        index = (index + (key - 1)) % n;
+        for (int i = index; i < n - 1; ++i)
+        {
+            array[i] = array[i + 1];
+        }
+        --n;
+    }
+    return array[0];
+}
+
+void test_massive(int key)
+{
+    for (int i = 2000; i < 100000; i += 2000)
+    {
+        int* main_array = new int[i];
+
+        for (int s = 0; s < i; s++)
+        {
+            main_array[s] = s;
+        }
+
+        auto begin = std::chrono::steady_clock::now();
+        decision_by_massives(main_array, i, key);
+        auto end = std::chrono::steady_clock::now();
+        auto time_span = std::chrono::duration_cast<std::chrono::microseconds>(end - begin);
+        std::cout << i << " " << time_span.count() << std::endl;
+
+        delete[] main_array;
+    }
+}
+
+int decision_by_list(LinkedList* head, int n, int key)
+{
+    LinkedList* tmp = head;
+    while (tmp != tmp->next)
+    {
+        for (int k = 0; k < key - 1; ++k)
+        {
+            tmp = tmp->next;
+        }
+        remove_after(tmp);
+    }
+    return tmp->data;
+}
+
+void test_list(int key)
+{
+    for (int i = 0; i < 1000000; i += 5000)
+    {
+        int first_node_data = 0;
+        LinkedList* first_node = insert_after(nullptr, first_node_data);
+        LinkedList* tail = first_node;
+        for (int s = 1; s < i; s++)
+        {
+            tail = insert_after(tail, s);
+        }
+        tail->next = first_node;
+            
+        auto begin = std::chrono::steady_clock::now();
+        decision_by_list(first_node, i, key);
+        auto end = std::chrono::steady_clock::now();
+        auto time_span = std::chrono::duration_cast<std::chrono::microseconds>(end - begin);
+        std::cout << i << " " << time_span.count() << std::endl;
+    }
+}
+
+void test_list_for_key()
+{
+    int count = 100000;
+
+    for (int key = 1; key < 50000; key += 2000)
+    {
+        int first_node_data = 0;
+        LinkedList* first_node = insert_after(nullptr, first_node_data);
+        LinkedList* tail = first_node;
+        for (int s = 1; s < count; s++)
+        {
+            tail = insert_after(tail, s);
+        }
+        tail->next = first_node;
+
+        auto begin = std::chrono::steady_clock::now();
+        decision_by_list(first_node, count, key);
+        auto end = std::chrono::steady_clock::now();
+        auto time_span = std::chrono::duration_cast<std::chrono::microseconds>(end - begin);
+        std::cout << key << " " << time_span.count() << std::endl;
+    }
+}
+
+int main()
+{
+    int count = 0;
+    int choose;
+    std::cout << "Enter 1 (for massive decision), 2 (for list decision): " << std::endl;
+    std::cin >> choose;
+    int key;
+    std::cout << "Enter index of soldiers, who would be killed: " << std::endl;
+    std::cin >> key;
+
+    if (choose == 1)
+    {
+        test_massive(key);
+    }
+    else if (choose == 2)
+    {
+        test_list_for_key();
+    }
+
+    return 0;
+}
